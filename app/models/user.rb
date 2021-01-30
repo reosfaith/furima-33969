83b66d04db09825966698aad28c48ae7f6f1f47e
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-01-30 15:32:29 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :password_confirmation, presence: true
+  validates :password,:password_confirmation,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  validates :family_name, :first_name, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: "must be zenkaku kanji-kana-hiragana"}
+  validates :family_name_kana, :first_name_kana, format:{with: /\A[ァ-ンー－]+\z/, message: "must be zenkaku kana" }
+
 end
