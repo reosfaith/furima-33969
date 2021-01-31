8bd63ad60a92960ab0457317fd51e102907490da
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-01-31 22:19:01 +0900

    ActiveStorageの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
 end
