950792574eba99b84ee095bce7e19725c0f9976e
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-01-31 13:47:28 +0900

    ルーティングの設定等

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :password_confirmation,
             presence: true
   validates :password, :password_confirmation,
