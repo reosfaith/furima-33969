a6db2f1b03ffc03f569db5bb57f4a1c543cbbc55
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-01 12:12:10 +0900

    ActiveHashの実装
    ActiveHashの実装とそれに伴ったビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to :category, :condition, :shippingcost ,:shippingarea, :shippingdate
   belongs_to :user
   has_one_attached :image
+
+  validates :product_name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_date_id, :image, presence: true
+  validates :category_id, :condition_id, :shipping_cost, numericality: { other_than: 1 }
 
   private
   def item_params
