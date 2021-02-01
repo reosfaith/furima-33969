fe7138572e7d1fb6968a630fa9fbb96b8e1892bf
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-01 15:00:13 +0900

    ActiveHashの実装 修正＃2
    ファイル名などの細かな修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Association::ActiveRecordExtensions
-  belongs_to :category, :condition, :shippingcost ,:shippingarea, :shippingdate
+  belongs_to :category, :condition, :shipping_cost ,:shipping_area, :shipping_date
   belongs_to :user
   has_one_attached :image
 
   validates :product_name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_date_id, :image, presence: true
+  validates :price,
   validates :category_id, :condition_id, :shipping_cost, numericality: { other_than: 1 }
 
   private
