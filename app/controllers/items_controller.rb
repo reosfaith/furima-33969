fd9b0100ebb15b463754637d9aaa55ac3e505002
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-03 14:16:28 +0900

    修正 ＃1
    ちょっとしたミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
   def move_to_index
     item = Item.find(params[:id])
     unless current_user.id == item.user.id
-      redirect_to action: index
+      redirect_to action: :index
     end
   end
 end
