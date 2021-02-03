b149401a1c19e8f86612b2b9dc773afdb1576e99
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-03 17:27:11 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    if  @item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       redirect_to item_path(params[:id])
+    end
   end
 
   private
