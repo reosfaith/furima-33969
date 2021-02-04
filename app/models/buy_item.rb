4a5a307317e1f3475b0aa144b34c6f205dd25a11
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-04 19:22:59 +0900

    buy_itemモデル作成・編集
    orderとdestinationモデルについて記述

--- app/models/buy_item.rb
+++ app/models/buy_item.rb
@@ -1,3 +1,15 @@
 class BuyItem
   include ActiveModel::Model
-  attr_accessor :
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user, :item
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\Z/ }
+    validates :prefecture_id, :city, :house_number, :phone_number, :user, :item
+  end
+
+  def save
+    Order.create(user_id: user.id, item_id: item.id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+  end
+
+end
