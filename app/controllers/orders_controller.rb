9ee3aa534ad617d0e4efa1a2e2716f4ee5e638de
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-05 12:29:42 +0900

    viewファイルの修正
    配送先部分の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @buy_item = BuyItem.new
   end
 
   def create
     @buy_item = BuyItem.new(item_params)
-
   end
 
   private
