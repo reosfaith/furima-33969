07a624ddbed6ba5ab3a8488f37339770a93ac2d8
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-02-06 14:01:02 +0900

    エラーハンドリングの部分実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :move_to_index
+  before_action :find_item, only: [:index, :create]
+  before_action :sold_out, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @buy_item = BuyItem.new
+    if @item.user.id == current_user.id
+      redirect_to :root
+    end
   end
 
   def create
     @buy_item = BuyItem.new(item_params)
-    if @buy_item.valid?
+    if @buy_item.valid? && @item.user.id != current_user.id
       @buy_item.save
       redirect_to item_orders_path(params[:item_id])
     else
@@ -22,5 +25,15 @@ class OrdersController < ApplicationController
 
   def item_params
     params.require(:buy_item).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :card_number, :card_exp_month, :card_exp_year, :card_cvc).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def sold_out
+    if @item.order.present?
+      redirect_to :root
+    end
   end
 end
